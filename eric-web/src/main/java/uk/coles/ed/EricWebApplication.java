da7a65a36e2c3103e590abb322b1b4873101260f
Ed Coles <edicoles@gmail.com>
2017-04-09 12:04:19 +0100

    Initial commit

---
+++ eric-web/src/main/java/uk/coles/ed/EricWebApplication.java
@@ -0,0 +1,12 @@
+package uk.coles.ed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EricWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EricWebApplication.class, args);
+	}
+}
---
+++ eric-web/src/test/java/uk/coles/ed/EricWebApplicationTests.java
@@ -0,0 +1,16 @@
+package uk.coles.ed;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EricWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
